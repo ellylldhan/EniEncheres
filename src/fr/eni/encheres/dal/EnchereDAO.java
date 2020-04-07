7066cab4188cdf49b3d729f1b7927305e0f54ad8
eperia35 <thomas.huet@hotmail.fr>
2020-04-07 15:51:51 +0200

    Update EnchereDAO.java
    ajout de commentaire

--- src/fr/eni/encheres/dal/EnchereDAO.java
+++ src/fr/eni/encheres/dal/EnchereDAO.java
@@ -18,8 +18,28 @@ import fr.eni.encheres.exception.DalException;
  */
 public interface EnchereDAO {
 
+	/**
+	 * Méthode en charge d'insérer un enregistrement dans la table Encheres
+	 * @param noArticle
+	 * @return
+	 * @throws DalException
+	 */
 	public void create(Enchere enchere)  throws DalException ;
+	
+	/**
+	 * Méthode en charge de récupérer l'instance d'une enchers qui a le montant le plus élevépour l'article donné en paramètre
+	 * @param noArticle
+	 * @return
+	 * @throws DalException
+	 */
 	public Enchere selectByIdArticleMustEnchere(int idArticle)  throws DalException ;
+	
+	/**
+	 * Méthode en charge de récupérer toute less instance d'objet d'enchère lié à un article
+	 * @param noArticle
+	 * @return
+	 * @throws DalException
+	 */
 	public List<Enchere> selectAllByIdArticle(int idArticle)  throws DalException ;
 
 	
