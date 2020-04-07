d9ea1e00c87243fcacf7ae1d6576a455b2175543
eperia35 <thomas.huet@hotmail.fr>
2020-04-07 11:44:57 +0200

    modification de gitignore et ajout de la daofactory et de connection provider

---
+++ src/fr/eni/encheres/dal/ConnectionProvider.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package fr.eni.encheres.dal;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+/**
+ * Classe en charge de
+ * @author thomas
+ * @version EniEncheres - v1.0
+ * @date 7 avr. 2020
+ */
+public class ConnectionProvider {
+
+	private static DataSource dataSource;
+	
+	/**
+	 * Au chargement de la classe, la DataSource est recherchée dans l'arbre JNDI
+	 */
+	static
+	{
+		Context context;
+		try {
+			context = new InitialContext();
+			ConnectionProvider.dataSource = (DataSource)context.lookup("java:comp/env/jdbc/pool_cnx");
+		} catch (NamingException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Impossible d'accéder à la base de données");
+		}
+	}
+	
+	/**
+	 * Cette méthode retourne une connection opérationnelle issue du pool de connexion
+	 * vers la base de données. 
+	 * @return
+	 * @throws SQLException
+	 */
+	public static Connection getConnection() throws SQLException
+	{
+		return ConnectionProvider.dataSource.getConnection();
+	}
+}
---
+++ src/fr/eni/encheres/dal/DAOFactory.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package fr.eni.encheres.dal;
+
+/**
+ * Classe en charge de
+ * @author thomas
+ * @version EniEncheres - v1.0
+ * @date 7 avr. 2020
+ */
+public class DAOFactory {
+
+	
+}
