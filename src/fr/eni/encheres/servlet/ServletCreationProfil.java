8b1b053911ecfd0995da4e942de04201c9316098
Loan PIROTAIS <pirotais.loan@gmail.com>
2020-04-14 10:03:52 +0200

    Page de modification de profil

--- src/fr/eni/encheres/servlet/ServletCreationProfil.java
+++ src/fr/eni/encheres/servlet/ServletCreationProfil.java
@@ -9,6 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fr.eni.encheres.bll.UtilisateurManager;
+import fr.eni.encheres.bo.Utilisateur;
+
 /**
  * Servlet implementation class CreationProfil
  */
@@ -28,6 +31,17 @@ public class ServletCreationProfil extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Utilisateur utilisateur = null;
+		boolean isConnected = true;
+		
+		UtilisateurManager utilisateurManager = UtilisateurManager.getInstance();
+		
+		//Simulation récupération de l'utilisateur connecté
+		utilisateur = utilisateurManager.getUtilisateur(4);
+		
+		request.setAttribute("utilisateur", utilisateur);
+		request.setAttribute("isConnected", isConnected);
+		
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/creation_profil.jsp");
 		rd.forward(request, response);
 	}
